5308a260f6fcd83ef4e5ca6c2fc4657d7bcaa143
John <jonnygovish@gmail.com>
2017-10-18 11:59:27 +0300

    Updating the page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app
-from .request import get_movies,get_movie
+from .request import get_movies,get_movie, search_movie
 
 @app.route('/')
 
@@ -12,7 +12,13 @@ def index():
 
 	print(popular_movies)
 	title="Home Welcome to the best movie review website online"
-	return render_template("index.html",title=title, popular= popular_movies,upcoming= upcoming_movie, now_showing = now_showing_movie)
+
+	search_movie = request.args.get('movie_query')
+
+	if search_movie:
+  		return redirect(url_for('search', movie_name = search_movie))
+	else: 
+		return render_template("index.html",title=title, popular= popular_movies,upcoming= upcoming_movie, now_showing = now_showing_movie)
 
 
 
@@ -23,3 +29,14 @@ def movie(id):
 	title=f"{movie.title}"
 
 	return render_template("movie.html",title=title,movie=movie)
+
+@app.route('/search/<movie_name>')
+def search(movie_name):
+	"""
+	View function to display the search results
+	"""
+	movie_name_list = movie_name.split(" ")
+	movie_name_format = "+".join(movie_name_list)
+	searched_movies = search_movie(movie_name_format)
+	title = f'search results for {movie_name}'
+	return render_template('search.html', movies = searched_movies)
