aea5d75aacde664c012d3a3926493fb62034827f
John <jonnygovish@gmail.com>
2017-10-18 15:10:31 +0300

    making updates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from flask import render_template, request, redirect, url_for
 from app import app
 from .request import get_movies,get_movie, search_movie
+from .models import reviews
+from .forms import ReviewForm
+Review = reviews.Review
+
+
 
 @app.route('/')
 
@@ -27,8 +32,8 @@ def movie(id):
 
 	movie=get_movie(id)
 	title=f"{movie.title}"
-
-	return render_template("movie.html",title=title,movie=movie)
+	reviews=Review.get_reviews(movie.id)
+	return render_template("movie.html",title=title,movie=movie,reviews=reviews)
 
 @app.route('/search/<movie_name>')
 def search(movie_name):
@@ -40,3 +45,19 @@ def search(movie_name):
 	searched_movies = search_movie(movie_name_format)
 	title = f'search results for {movie_name}'
 	return render_template('search.html', movies = searched_movies)
+
+@app.route('/movie/review/new/<int:id>', methods = ['GET', 'POST'])
+def new_review(id):
+	form = ReviewForm()
+	movie = get_movie(id)
+
+	if form.validate_on_submit():
+		title = form.title.data
+		review = form.review.data
+		new_review = Review(movie.id, title, movie.image, review)
+		new_review.save_review()
+		return redirect(url_for('movie', id = movie.id))
+
+	title = f'{movie.title} review'
+	return render_template('new_review.html', title = title, review_form = form, movie = movie )
+
